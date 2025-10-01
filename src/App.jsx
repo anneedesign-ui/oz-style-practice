86b86d5c679deb747385076906f06163047aa19a
anneedesign-ui <anneedesign@gmail.com>
2025-10-01 12:49:05 +0900

    [React] 11일차 과제: SCSS를 Styled-Components로 변환 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import "./App.scss";
+import styled from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+// Styled Components 정의
+const MainContainer = styled.main`
+  /* App.scss의 전역 스타일이 필요하다면 여기에 추가 */
+`;
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
+
 function App() {
   return (
-    <main>
+    <MainContainer>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
-    </main>
+      </Section>
+    </MainContainer>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,48 @@
+import styled from "styled-components";
+
+// Styled Components 정의
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
+  background-color: black;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const MenuContainer = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 400;
+  list-style: none;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    color: #d7fa00;
+  }
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+    <HeaderContainer>
+      <Title>OZ코딩스쿨</Title>
+      <MenuContainer>
+        <MenuItem>로그인</MenuItem>
+        <MenuItem>회원가입</MenuItem>
+        <MenuItem>내클래스</MenuItem>
+      </MenuContainer>
+    </HeaderContainer>
   );
 }
